c38ae136f02851ed6be72c9feabecfa88f1a6937
unknown <okwuosahpaschal@gmail.com>
2022-04-01 23:22:24 +0100

    first commit

--- truffle-config.js
+++ truffle-config.js
@@ -3,6 +3,12 @@ const path = require("path");
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  compilers: {
+    solc: {
+      version: '0.8.0',
+      parser: 'solcjs'
+    }
+  },
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     develop: {
