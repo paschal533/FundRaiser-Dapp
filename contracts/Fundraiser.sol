a537763e74782525827f268d72ce3563c1ac36f4
unknown <okwuosahpaschal@gmail.com>
2022-04-06 15:04:15 -0700

    update commit

--- contracts/Fundraiser.sol
+++ contracts/Fundraiser.sol
@@ -1,8 +1,16 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "openzeppelin-solidity/contracts/access/Ownable.sol";
 
 contract Fundraiser is Ownable {
+  struct Donation {
+    uint256 value;
+    //uint256 conversionFactor;
+    uint256 date;
+  }
+  mapping(address => Donation[]) private _donations;
+
   string public name;
   string public url;
   string public imageURL;
@@ -26,5 +34,39 @@ contract Fundraiser is Ownable {
     description = _description;
     beneficiary = _beneficiary;
     _transferOwnership(_custodian);
+  }
+
+  function setBeneficiary(address payable _beneficiary) public onlyOwner {
+    beneficiary = _beneficiary;
+  }
+
+  function myDonationsCount() public view returns(uint256) {
+    return _donations[msg.sender].length;
+  }
+
+  function donate() public payable {
+    Donation memory donation = Donation({
+      value: msg.value,
+      date: block.timestamp
+    });
+    _donations[msg.sender].push(donation);
+  }
+
+  function myDonations() public view returns(
+    uint256[] memory values,
+    uint256[] memory dates
+  )
+  {
+    uint256 count = myDonationsCount();
+    values = new uint256[](count);
+    dates = new uint256[](count);
+
+    for (uint256 i = 0; i < count; i++) {
+      Donation storage donation = _donations[msg.sender][i];
+      values[i] = donation.value;
+      dates[i] = donation.date;
+    }
+
+    return (values, dates);
   }
 }
--- test/fundraiser_test.js
+++ test/fundraiser_test.js
@@ -9,17 +9,18 @@ contract("Fundraiser", accounts => {
   const beneficiary = accounts[1];
   const owner = accounts[0];
 
+  beforeEach ( async () => {
+    fundraiser = await FundraiserContract.new(
+      name,
+      url,
+      imageURL,
+      description,
+      beneficiary,
+      owner
+    );
+  });
+
   describe("initialization", () => {
-    beforeEach ( async () => {
-      fundraiser = await FundraiserContract.new(
-        name,
-        url,
-        imageURL,
-        description,
-        beneficiary,
-        owner
-      );
-    });
     it("gets the beneficiary name", async () => {
       const actaul = await fundraiser.name();
       assert.equal(actaul, name, "name should match");
@@ -50,4 +51,47 @@ contract("Fundraiser", accounts => {
       assert.equal(actaul, owner, "owner should match");
     });
   });
+
+  describe("setBeneficiary", () => {
+    const newBeneficiary = accounts[2];
+
+    it("update beneficiary when called by owner account", async () => {
+      await fundraiser.setBeneficiary(newBeneficiary, {from: owner});
+      const actualBeneficiary = await fundraiser.beneficiary();
+      assert.equal(actualBeneficiary, newBeneficiary, "beneficiary should match");
+    });
+
+    it("throws an error when called from a non-owner account", async () => {
+      try {
+        await fundraiser.setBeneficiary(newBeneficiary, {from: accounts[3]});
+        assert.fail("withdraw was not restricted to owners")
+      } catch(err) {
+        const expectedError = "Ownable: caller is not the owner"
+        const actualError = err.reason;
+        assert.equal(actualError, expectedError, "should not be permitted")
+      }
+    })
+  })
+
+  describe("making donations", () => {
+    const value = web3.utils.toWei("0.0289");
+    const donor = accounts[2];
+
+    it("increase myDonationsCount", async () => {
+      const currentDonationsCount = await fundraiser.myDonationsCount({from: donor});
+
+      await fundraiser.donate({from: donor, value});
+
+      const newDonationsCount = await fundraiser.myDonationsCount({from: donor});
+
+      assert.equal(1, newDonationsCount - currentDonationsCount, "myDonationsCount should increment by 1");
+    });
+    it("includes donation in myDonations", async () => {
+      await fundraiser.donate({from: donor, value});
+      const {values, dates} = await fundraiser.myDonations({from: donor});
+
+      assert.equal(value, values[0], "values should match");
+      assert(dates[0], "date should be present");
+    })
+  })
 });
