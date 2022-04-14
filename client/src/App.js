b8ca3483ff92349e1c5128368679cde24b19e041
unknown <okwuosahpaschal@gmail.com>
2022-04-14 14:47:42 -0700

    update commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,31 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import FactoryContract from "./contracts/FundraiserFactory.json";
 import getWeb3 from "./getWeb3";
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { BrowserRouter as Router, Route, Routes, NavLink } from "react-router-dom";
+
+import NewFundraiser from './NewFundraiser'
+import Home from './Home'
+//import Receipts from './Receipts'
 
 import "./App.css";
 
 const App = () => {
-  const [state, setState] = useState({
-    web3: null, accounts: null, contract: null
-  });
-  const [storageValue, setStorageValue] = useState(0);
+  const [state, setState] = useState({web3: null, accounts: null, contract: null});
 
   useEffect(() => {
-    const init = async () => {
+    const init = async() => {
       try {
+        // Get network provider and web3 instance.
         const web3 = await getWeb3();
+
+        // Use web3 to get the user's accounts.
         const accounts = await web3.eth.getAccounts();
+
+        // Get the contract instance.
         const networkId = await web3.eth.net.getId();
         const deployedNetwork = FactoryContract.networks[networkId];
         const instance = new web3.eth.Contract(
@@ -22,23 +33,51 @@ const App = () => {
           deployedNetwork && deployedNetwork.address,
         );
 
+        // Set web3, accounts, and contract to the state, and then proceed with an
         setState({web3, accounts, contract: instance});
+
       } catch(error) {
-        console.log("failed to load web3", error);
+        // Catch any errors for any of the above operations.
+        alert(
+          `Failed to load web3, accounts, or contract. Check console for details.`,
+        );
+        console.error(error);
       }
     }
     init();
   }, []);
 
+  const useStyles = makeStyles({
+    root: {
+      flexGrow: 1,
+    },
+  });
+
   const runExample = async () => {
     const { accounts, contract } = state;
   };
 
-  return(
+  return (
     <div>
-      <h1>Fundraiser</h1>
+      <Router>
+        <AppBar position="static" color="default" style={{ margin: 0 }}>
+          <Toolbar>
+           <Typography variant="h6" color="inherit">
+             <NavLink className="nav-link" to="/">Home</NavLink>
+           </Typography>
+           <NavLink className="nav-link" to="/new/">{NewFundraiser}</NavLink>
+          </Toolbar>
+       </AppBar>
+        <Routes>
+          <Route path="/" exact component={Home} />
+          <Route path="/new/" component={NewFundraiser} />
+        {/*<Route path="/receipts" component={Receipts} />*/}
+        {NewFundraiser}
+        </Routes>
+      </Router>
     </div>
   )
 }
+
 
 export default App;
