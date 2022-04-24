1153708338bc9bcc0ce2ee6f2e6af38a952d2bd1
unknown <okwuosahpaschal@gmail.com>
2022-04-24 13:02:28 -0700

    update commit

--- client/src/Home.js
+++ client/src/Home.js
@@ -30,7 +30,7 @@ const Home = () => {
       const networkId = await web3.eth.net.getId();
       const deployedNetwork = FactoryContract.networks[networkId];
       const accounts = await web3.eth.getAccounts();
-      const instance = new web3.eth.Contract(
+      //const instance = new web3.eth.Contract(
         FactoryContract.abi,
         deployedNetwork && deployedNetwork.address,
       );
