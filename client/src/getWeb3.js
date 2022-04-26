7c9b9a0927e8bda77818331cb454107481d78840
unknown <okwuosahpaschal@gmail.com>
2022-04-26 13:53:24 -0700

    update commit

--- client/src/getWeb3.js
+++ client/src/getWeb3.js
@@ -29,7 +29,7 @@ const getWeb3 = () =>
           "http://127.0.0.1:8545"
         );
         const web3 = new Web3(provider);
-        //console.log("No web3 instance injected, using Local web3.");
+        console.log("No web3 instance injected, using Local web3.");
         resolve(web3);
       }
     });
